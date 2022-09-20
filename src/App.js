def7fa9719af7a324e25e8df6e7f79370a32c583
Pan <pancanugrahawicaksana@gmail.com>
2022-09-21 03:32:22 +0700

    Simple Authentication with Auth0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginButton from './components/loginButton';
+import LogoutButton from './components/logoutButton';
+import UserProfile from './components/userProfile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <LoginButton/>
+      <LogoutButton/>
+      <p>Detail User Information if Authenticated</p>
+      <UserProfile/>
+    </div>    
   );
 }
 
